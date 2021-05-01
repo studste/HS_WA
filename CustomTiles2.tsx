ac921305f89b64f17e50d668e73153a337556fac
stde0006 <stde0006@stud.hs-kl.de>
2021-05-01 13:28:11 +0200

    Sonstiges

---
+++ CustomTiles2.tsx
@@ -0,0 +1,10 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.6.0" name="CustomTiles" tilewidth="32" tileheight="32" tilecount="1" columns="1">
+ <grid orientation="orthogonal" width="64" height="32"/>
+ <image source="CustomTiles.PNG" trans="ffffff" width="60" height="35"/>
+ <tile id="0">
+  <properties>
+   <property name="Collision" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
